08b52a1fd3ebbfad591f4911f9700204ca8e9f52
Gustavo Sandoval <jgusta.s25@gmail.com>
2021-05-28 22:57:56 -0300

    Se elimino main.js y se implementan las validaciones en el login mediante los formularios reactivos.

--- frontend/src/app/auth/pages/login/login.component.ts
+++ frontend/src/app/auth/pages/login/login.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  //Se crea el formulario con su nombre y validadores.
+  formulario: FormGroup = this.fb.group({
+    email: ['test@gmail.com', [Validators.required]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  //Verificar si el campo tiene errores cuando se ha movido de  el
+  campoEsValido(campo: string) {
+    return (
+      this.formulario.controls[campo].errors && this.formulario.controls[campo].touched
+    );
   }
 
+  //Funcion de login
+  login() {
+    console.log(this.formulario.value);
+    console.log(this.formulario.valid);
+  }
 }
